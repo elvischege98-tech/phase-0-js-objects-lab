de514349ab14f282249a683ea3d784c161fa9c7c
Elvis Chege <elvischege98@gmail.com>
2026-03-23 13:02:28 +0300

    Returned the statement "Needed for the tests to work"

--- index.js
+++ index.js
@@ -50,13 +50,13 @@ function addCheckedInProperty(attendee) {
 
 addCheckedInProperty(attendee);
 
-// //Needed for the tests to work. Don't modify
-// module.exports = {
-//   ...(typeof attendee !== 'undefined' && { attendee }),
-//   ...(typeof logAttendeeName !== 'undefined' && { logAttendeeName }),
-//   ...(typeof logTicketPrice !== 'undefined' && { logTicketPrice }),
-//   ...(typeof updateTicketType !== 'undefined' && { updateTicketType }),
-//   ...(typeof updateTicketPrice !== 'undefined' && { updateTicketPrice }),
-//   ...(typeof removeEventProperty !== 'undefined' && { removeEventProperty }),
-//   ...(typeof addCheckedInProperty !== 'undefined' && { addCheckedInProperty })
-// };
+//Needed for the tests to work. Don't modify
+module.exports = {
+  ...(typeof attendee !== 'undefined' && { attendee }),
+  ...(typeof logAttendeeName !== 'undefined' && { logAttendeeName }),
+  ...(typeof logTicketPrice !== 'undefined' && { logTicketPrice }),
+  ...(typeof updateTicketType !== 'undefined' && { updateTicketType }),
+  ...(typeof updateTicketPrice !== 'undefined' && { updateTicketPrice }),
+  ...(typeof removeEventProperty !== 'undefined' && { removeEventProperty }),
+  ...(typeof addCheckedInProperty !== 'undefined' && { addCheckedInProperty })
+};
